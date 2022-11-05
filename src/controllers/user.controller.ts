2fd0f78bb00bd2c8c7af6ceec0d5e858432b4a5a
Jean Egalon <egalon.jean@gmail.com>
2022-11-05 08:21:10 -0300

    feito o requisito 7

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,14 +1,23 @@
 import { Request, Response } from 'express';
+import {
+  validateClasse, validateLevel,
+  validatePassword, validateUsername,
+} from '../middlewares/validates';
 import UserService from '../services/user.service';
 
 export default class UserController {
   userService = new UserService();
 
   async createUser(req: Request, res: Response) {
+    const { username, classe, level, password } = req.body; 
+    validateUsername(username);
+    validateClasse(classe);
+    validateLevel(level);
+    validatePassword(password);
     const user = await this.userService.createUser(req.body);
-    const { username } = user;
+    const nome = user.username;
     
-    const newToken = await this.userService.createToken(username);
+    const newToken = await this.userService.createToken(nome);
     
     res.status(201).json({ token: newToken });
   }
--- src/middlewares/validates.ts
+++ src/middlewares/validates.ts
@@ -27,3 +27,59 @@ export const validateAmount = (amount: string) => {
     throw new HttpException(422, '"amount" length must be at least 3 characters long');
   }
 };
+
+export const validateUsername = (username: string) => {
+  if (!username) {
+    throw new HttpException(400, '"username" is required');
+  }
+
+  if (typeof username !== 'string') {
+    throw new HttpException(422, '"username" must be a string');
+  }
+
+  if (username.length < 3) {
+    throw new HttpException(422, '"username" length must be at least 3 characters long');
+  }
+};
+
+export const validateClasse = (classe: string) => {
+  if (!classe) {
+    throw new HttpException(400, '"classe" is required');
+  }
+
+  if (typeof classe !== 'string') {
+    throw new HttpException(422, '"classe" must be a string');
+  }
+
+  if (classe.length < 3) {
+    throw new HttpException(422, '"classe" length must be at least 3 characters long');
+  }
+};
+
+export const validateLevel = (level: number) => {
+  if (level <= 0) {
+    throw new HttpException(422, '"level" must be greater than or equal to 1');
+  }
+
+  if (!level) {
+    throw new HttpException(400, '"level" is required');
+  }
+
+  if (typeof level !== 'number') {
+    throw new HttpException(422, '"level" must be a number');
+  }  
+};
+
+export const validatePassword = (password: string) => {
+  if (!password) {
+    throw new HttpException(400, '"password" is required');
+  }
+
+  if (typeof password !== 'string') {
+    throw new HttpException(422, '"password" must be a string');
+  }
+
+  if (password.length < 8) {
+    throw new HttpException(422, '"password" length must be at least 8 characters long');
+  }
+};
